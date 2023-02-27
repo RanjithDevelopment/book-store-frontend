7bd5e9faef777a95bcd0eb097219e9783df62b69
RANJITH <73814077+RanjithDevelopment@users.noreply.github.com>
2023-02-27 13:19:18 +0530

    Add files via upload

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,10 @@
 import { Box, Typography } from '@mui/material'
 import React from 'react'
-
+import Header from "./Header";
 const About = () => {
   return (
     <div>
+      <Header/>
 <Box display='flex' flexDirection='column' alignItems='center'>
   <Typography sx={{fontFamily:"fantasy"}} variant='h2'>This Library Managemment Application</Typography>
   <Typography  sx={{fontFamily:"fantasy"}} variant='h3'>This is A Simple CRUD Application Developed Using Mern Stack</Typography>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,43 +5,40 @@ import { NavLink } from 'react-router-dom';
 import jwt_decode from "jwt-decode";
 const Header = () => {
     const token = localStorage.getItem("token");
-const existuser = jwt_decode(token);
+    const existuser = jwt_decode(token);
+   
 
-    const [value, setValue] = useState();
+    const [value, setValue] = useState(4);
     return (
         <div>
-            <AppBar sx={{backgroundColor:"#232F3D"}} position='sticky'>
+            <AppBar sx={{ backgroundColor: "#232F3D" }} position='sticky'>
                 <Toolbar>
-                    <NavLink to='/Header' style={{color:"white"}}>
-                    <Typography>
-                        <LibraryBooksIcon /> ADMIN DASHBOARD
-                    </Typography>
+                    <NavLink to='/Header' style={{ color: "white" }}>
+                        <Typography>
+                            <LibraryBooksIcon /> ADMIN DASHBOARD
+                        </Typography>
                     </NavLink>
-                    <Tabs 
-                    sx={{ml:"auto"}}
-                    textColor='inherit' 
-                    indicatorColor='primary' 
-                    value={value} 
-                    onChange={(e, val) => setValue(val)}
+                    <Tabs
+                        sx={{ ml: "auto" }}
+                        textColor='inherit'
+                        indicatorColor='primary'
+                        value={value}
+                        onChange={(e, val) => setValue(val)}
                     >
-                       
+
+
+                        <Tab LinkComponent={NavLink} to="/add" label='ADD Book' />
+                        <Tab LinkComponent={NavLink} to="/books" label='Books' />
+                        <Tab LinkComponent={NavLink} to="/about" label='About US' />
+                        <Tab LinkComponent={NavLink} to="/Signup" label='SignUp !' />
+
+                    
+   
                         
-                        <Tab LinkComponent={NavLink } to="/add" label='ADD Book' />
-                        <Tab LinkComponent={NavLink } to="/books" label='Books' />
-                        <Tab LinkComponent={NavLink } to="/about" label='About US' />
-                        <li>
-                        {
-                        token ? <></> :<Tab LinkComponent={NavLink } to="/" label='Login' />
-                        }
-                        </li>
-                        <li>
-                        {
-                        token ? <></> :<Tab LinkComponent={NavLink } to="/Signup" label='Signup' />
-                        }
-                        </li>
+
                     </Tabs>
                     <Typography>
-                        {existuser.firstname} {existuser.lastname}
+                       User Name: {existuser.firstname} {existuser.lastname}
                     </Typography>
                 </Toolbar>
 
